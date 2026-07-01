64cf364ed7140f04b51169e50a7325723a2f77e2
Traxeltz <165699044+Traxeltz@users.noreply.github.com>
2026-07-01 04:09:35 -0300

    build: Update C++ default version to C++20

--- back-end/internals/core.cc
+++ back-end/internals/core.cc
@@ -1,8 +1,8 @@
 #include <algorithm>
 #include <vector>
 
-#include "core.h"
 #include "../schedulers/fifo.h"
+#include "../schedulers/rr.h"
 #include "../schedulers/sjf.h"
 #include "../schedulers/priority.h"
 #include "../schedulers/edf.h"
@@ -22,7 +22,7 @@ void Simulator::run(const payload &p) {
       SJFScheduler::run(p, &result.timeline);
     break;
   case algorithms::RR:
-    // TODO
+    RRScheduler::run(p, &result.timeline);
     break;
   case algorithms::PRIO:
     PriorityScheduler::run(p, &result.timeline);
@@ -70,12 +70,11 @@ void Simulator::calculate_stats(const std::vector<process> &vp, const payload *p
     int ta = finish - p.absolute_arrival_time;
     int r = firstExecution - p.absolute_arrival_time;
 
-    process_stat ps;
-    ps.id = p.id;
-    ps.waiting_time = w;
-    ps.turnaround_time = ta;
-    ps.response_time = r;
-    ps.finish_time = finish;
+    process_stat ps = {.id = p.id,
+                       .waiting_time = w,
+                       .turnaround_time = ta,
+                       .response_time = r,
+                       .finish_time = finish};
     result.process_stats.emplace_back(ps);
   }
 
